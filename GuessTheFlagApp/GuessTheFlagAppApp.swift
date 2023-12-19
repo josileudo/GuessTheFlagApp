67cac99132d5e8cbdb0ffac75a42acf0ba44606c
Josileudo <josileudorodrigues75@gmail.com>
2023-12-19 16:50:43 -0300

    Initial Commit

---
+++ GuessTheFlagApp/GuessTheFlagAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagAppApp.swift
+//  GuessTheFlagApp
+//
+//  Created by Josileudo on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
