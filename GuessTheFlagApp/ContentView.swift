3319410e39f49ac7ad3f15e1a28a1ac26d36d5f4
Josileudo <josileudorodrigues75@gmail.com>
2023-12-26 10:06:51 -0300

    :memo: label: Add new readme

--- GuessTheFlagApp/ContentView.swift
+++ GuessTheFlagApp/ContentView.swift
@@ -8,25 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "Uk", "Ukraine", "US"]
+    @State private var correctAnswers = Int.random(in: 0...2)
+    
     @State private var showAlert: Bool = false
+    @State private var textResponse: String = ""
+    @State private var score: Int = 0
     
     var body: some View {
-        VStack {
-            Button("Button 1", action: clickMe)
-                .buttonStyle(.bordered)
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.5, green: 0.3, blue: 0.8), location: 0.3),
+                .init(color: Color(red: 0.9, green: 0.15, blue: 0.16), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            
+            VStack {
+                HStack(alignment: .top) {
+                    Text("Score: ")
+                        .foregroundColor(.white)
+                        .font(.largeTitle.weight(.bold))
+                    
+                    Text("\(score)")
+                        .foregroundColor(.white)
+                        .font(.largeTitle.weight(.bold))
+                }
                 
-        }
-        .alert("Hello peoples", isPresented: $showAlert) {
-            Button("Close", role: .destructive) {}
-            Button("Cancel", role: .cancel) {}
-        } message: {
-            Text("Your phone has low getter")
+                Spacer()
+                
+                VStack(spacing: 30) {
+    
+                    VStack() {
+                        Text("Tap your Flag: ")
+                            .foregroundColor(.white)
+                            .font(.subheadline.weight(.semibold))
+    
+                        Text(countries[correctAnswers])
+                            .foregroundColor(.white)
+                            .font(.largeTitle.weight(.bold))
+    
+                    }
+    
+                    ForEach(0..<3) { index in
+                        Button{
+                            tappedFlag(index)
+                        } label: {
+                            Image(countries[index])
+                                .cornerRadius(90)
+                                .shadow(radius: 5)
+                        }
+                    }
+    
+                }
+                .alert(textResponse, isPresented: $showAlert) {
+                    Button("Continue", action: {
+                        randomValues()
+                    })
+                } message: {
+                     Text("Your score is ?? ")
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(Rectangle())
+                .cornerRadius(20)
+                .padding(.horizontal, 20)
+                
+                Spacer()
+            }
         }
     }
     
-    func clickMe() {
-        print("Hello word")
+    func tappedFlag(_ number: Int) {
+        if number == correctAnswers {
+            textResponse = "Correct ✅"
+            score += 1
+        } else {
+            textResponse = "Wrong ❌"
+        }
+        
         showAlert = true
+    }
+    
+    func randomValues() {
+        countries = countries.shuffled()
+        correctAnswers = Int.random(in: 0...2)
     }
 }
 
---
+++ README.md
@@ -0,0 +1,10 @@
+# GuessTheFlagApp
+
+<img src="assets/screenshot.gif" height="600" title="project based">
+
+## Description
+This principal goal of the project is solver second challenge of the Paul Hudson courses
+
+<div align="center">
+  <small> Josileudo Rodrigues - 2023</small>
+</div
